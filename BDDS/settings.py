7c4f908f9fa20c0433cc9f964c9785a0e2abffd3
shwetapardhiskillpilots <shweta_pardhi@skillpilots.com>
2026-02-27 17:02:18 +0530

    Fixed STATIC_URL, normalized static paths, and added CSRF trusted origins to resolve dashboard issues

--- BDDS/settings.py
+++ BDDS/settings.py
@@ -35,6 +35,11 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    'http://192.168.1.33:8000',
+    'http://127.0.0.1:8000',
+    'http://0.0.0.0:8000',
+]
 
 
 # Application definition
@@ -157,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS=[ STATIC_DIR]
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
